ad8402d62f7616e709186c69142d95ef0c6f6153
gymnet-fitness <liz@gymnet-fitness.com>
2023-02-02 15:28:17 -0500

    Update FAQPage.js

--- src/containers/FAQPage/FAQPage.js
+++ src/containers/FAQPage/FAQPage.js
@@ -26,20 +26,32 @@ const FAQPage = () => {
 
           <div>
             <h3>Where is GymNet available?</h3>
-            <p>Answer: GymNet currently operates in the United States, with focused roll-outs in Atlanta, New York, and Los Angeles. Check back soon on new hosts in other areas.</p>
+            <p>GymNet currently operates in the United States, with focused roll-outs in Atlanta, New York, and Los Angeles. Check back soon on new hosts in other areas.</p>
           </div>
           <div>
             <h3>What safety measures are in place?</h3>
-            <p>Answer: We take the safety of our hosts and members very seriously at GymNet. Ensuring that everyone involved in a workout session is protected in the case of unexpected issues. Currently, all hosts are strongly recommended to have active renters' insurance. GymNet is working on expanding site-wide insurance for all parties for long-term coverage. </p>
+            <p>We take the safety of our hosts and members very seriously at GymNet. Currently, we strongly recommended all hosts to have active renters' insurance to protect them in the case of unexpected issues.</p>
+            <p>In the near future, GymNet plans to provide liability and property insurance for hosts and members automatically as part of our services.</p>
+          </div>
+          <div>
+            <h3>Can hosts control who books with them?</h3>
             <p>All Hosts have the authority to approve or deny booking requests and message with members prior, during, and after the session.</p>
           </div>
           <div>
             <h3>How do reviews work?</h3>
-            <p>Answer: Both hosts and members have the opportunity to review each other after a session is completed. Members and hosts can view each other's reviews prior to booking a session.</p>
+            <p>Both hosts and members have the opportunity to review each other after a session is completed. Members and hosts can view each other's reviews prior to booking a session.</p>
           </div>
           <div>
             <h3>As a member, when am I charged for a session?</h3>
-            <p>Answer: Members are charged at the time the host approves the requested session. Depending on your payment method, the charge may not be reflected immediately in your financial account.</p>
+            <p>Members are charged at the time the host approves the requested session. Depending on your payment method, the charge may not be reflected immediately in your financial account.</p>
+          </div>
+          <div>
+            <h3>How much does GymNet cost?</h3>
+            <p>Anyone can join the GymNet platform, free of charge. GymNet charges a small service fee for each approved booking that takes place, similar to other marketplace applications.</p>
+          </div>
+          <div>
+            <h3>What types of workouts does GymNet have?</h3>
+            <p>All types of workouts and specialties are welcome on GymNet! Are you a personal trainer? Do you teach swim lessons? Do you have a home gym, or want to expand your fitness studio's customer base? Then GymNet is for you! We are constantly adding new workout categories to our site to support our hosts and members.</p>
           </div>
         </LayoutWrapperMain>
 
