88a3f3c0c904f2ad2bd59056624fe14263f43320
gymnet-fitness <liz@gymnet-fitness.com>
2023-02-03 13:44:09 -0500

    Update default-location-searches.js

--- src/default-location-searches.js
+++ src/default-location-searches.js
@@ -13,8 +13,8 @@ const defaultLocations = [
     predictionPlace: {
       address: 'Atlanta, Georgia, USA',
       bounds: new LatLngBounds(
-          new LatLng(33.659131000000, -84.526561000000),
-          new LatLng(33.881738000000, -84.348033000000)
+          new LatLng(33.659131000001, -84.526561000001),
+          new LatLng(33.881738000001, -84.348033000001)
       ),
     },
   },
