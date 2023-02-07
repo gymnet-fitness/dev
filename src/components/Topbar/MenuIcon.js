1b7274f02f68eab3ac22bd54ec7dd063c2fab476
gymnet-fitness <liz@gymnet-fitness.com>
2023-02-07 12:27:06 -0500

    Update MenuIcon.js

--- src/components/Topbar/MenuIcon.js
+++ src/components/Topbar/MenuIcon.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
-
+import { IconPng } from '/public/static/icons/android-chrome-384x384.png'
 import css from './Topbar.module.css';
 
 const MenuIcon = props => {
@@ -9,19 +9,7 @@ const MenuIcon = props => {
   const classes = classNames(rootClassName || css.rootMenuIcon, className);
 
   return (
-    <svg
-      className={classes}
-      width="18"
-      height="12"
-      viewBox="0 0 18 12"
-          xmlns="/public/static/icons/android-chrome-384x384.png"
-    >
-      <g fillRule="evenodd">
-        <rect width="18" height="2" rx="1" />
-        <rect y="5" width="18" height="2" rx="1" />
-        <rect y="10" width="18" height="2" rx="1" />
-      </g>
-    </svg>
+      { IconPng }
   );
 };
 
