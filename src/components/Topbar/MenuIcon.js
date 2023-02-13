fd1228108d9b99ca851ef2689c8c925d1b6675a0
gymnet-fitness <liz@gymnet-fitness.com>
2023-02-13 15:08:57 -0500

    Attempting to change the mobile icon

--- src/components/Topbar/MenuIcon.js
+++ src/components/Topbar/MenuIcon.js
@@ -1,28 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
+import Logo from './logo.png';
 
 import css from './Topbar.module.css';
+
+
 
 const MenuIcon = props => {
   const { className, rootClassName } = props;
   const classes = classNames(rootClassName || css.rootMenuIcon, className);
 
-  return (
-    <svg
-      className={classes}
-      width="18"
-      height="12"
-      viewBox="0 0 18 12"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <g fillRule="evenodd">
-        <rect width="18" height="2" rx="1" />
-        <rect y="5" width="18" height="2" rx="1" />
-        <rect y="10" width="18" height="2" rx="1" />
-      </g>
-    </svg>
-  );
+    console.log(Logo);
+    return <img src={logo} alt="Logo" />;
 };
 
 const { string } = PropTypes;
