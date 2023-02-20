9a7b90df8bc479bfb69455ff54b23124b181cb76
gymnet-fitness <liz@gymnet-fitness.com>
2023-02-20 10:07:21 -0500

    Update SectionClasstypeMaybe.js

--- src/containers/ListingPage/SectionClasstypeMaybe.js
+++ src/containers/ListingPage/SectionClasstypeMaybe.js
@@ -2,23 +2,29 @@
 import { FormattedMessage } from '../../util/reactIntl';
 
 // Import css from existing CSS Modules file:
+
 import css from './ListingPage.module.css';
 
 // Create new React component
+
 const SectionClasstypeMaybe = props => {
-  // Component's props should include all the possible options (from config)
+
+    // Component's props should include all the possible options (from config)
   // and listing's publicData
+
   const { options, publicData } = props;
   const selectedOption =
     publicData && publicData.classtype ? publicData.classtype : null;
 
   // Don't return anything if public data doesn't contain classtype field
   // That's why we named this component as SectionClasstypeMaybe
-  if (!publicData || !selectedOption) {
+
+    if (!publicData || !selectedOption) {
     return null;
   }
 
   // Find selected options label
+
   const optionConfig = options.find(o => o.key === selectedOption);
   const optionLabel = optionConfig ? optionConfig.label : null;
   return (
@@ -26,7 +32,7 @@ const SectionClasstypeMaybe = props => {
           <h2 className={css.featuresTitle}>
         <FormattedMessage
           id="ListingPage.classtypeTitle"
-          values={{ view: optionLabel.toLowerCase() }}
+          values={{ classtype: optionLabel.toLowerCase() }}
         />
       </h2>
     </div>
