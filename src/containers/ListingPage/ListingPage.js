c82de13241c52d0db550a83e07aa606b23f2d224
gymnet-fitness <liz@gymnet-fitness.com>
2023-02-22 14:17:39 -0500

    trying again

--- src/containers/ListingPage/ListingPage.js
+++ src/containers/ListingPage/ListingPage.js
@@ -59,7 +59,7 @@ import SectionDescriptionMaybe from './SectionDescriptionMaybe';
 import SectionFeaturesMaybe from './SectionFeaturesMaybe';
 import SectionReviews from './SectionReviews';
 import SectionRecognitionMaybe from './SectionRecognitionMaybe';
-import SectionClasstypeMaybe from './SectionClasstypeMaybe';
+import SectionclasstypeMaybe from './SectionclasstypeMaybe';
 import SectionMapMaybe from './SectionMapMaybe';
 import css from './ListingPage.module.css';
 
@@ -446,7 +446,7 @@ export class ListingPageComponent extends Component {
                   />
                   
                                 <SectionDescriptionMaybe description={description} />
-                  <SectionClasstypeMaybe options={classtypeOptions} publicData={publicData} />
+                  <SectionclasstypeMaybe options={classtypeOptions} publicData={publicData} />
                                 <SectionFeaturesMaybe options={yogaStylesOptions} publicData={publicData} />
                                 <SectionRecognitionMaybe options={recognitionOptions} publicData={publicData} />
                                 
--- src/containers/ListingPage/SectionClasstypeMaybe.js
+++ src/containers/ListingPage/SectionClasstypeMaybe.js
@@ -1,29 +1,36 @@
 ﻿import React from 'react';
 import { FormattedMessage } from '../../util/reactIntl';
 
-
+// Import css from existing CSS Modules file:
 import css from './ListingPage.module.css';
 
-const SectionClasstypeMaybe = props => {
-    const { options, publicData } = props;
-    if (!publicData) {
-        return null;
-    }
+// Create new React component
+const SectionclasstypeMaybe = props => {
+  // Component's props should include all the possible options (from config)
+  // and listing's publicData
+  const { options, publicData } = props;
+  const selectedOption =
+    publicData && publicData.classtype ? publicData.classtype : null;
 
-  const selectedOptions = publicData && publicData.classtype ? publicData.classtype : [];
-    const selectedConfigOptions = options.filter(o => selectedOptions.find(s => s === o.key));
+  // Don't return anything if public data doesn't contain classtype field
+  // That's why we named this component as SectionclasstypeMaybe
+  if (!publicData || !selectedOption) {
+    return null;
+  }
 
-    return (
-        <div className={css.sectionFeatures}>
-            <h2 className={css.featuresTitle}>
-          <FormattedMessage id="ListingPage.classtypeTitle"
-            options={selectedConfigOptions}
-            selectedOptions={selectedOptions}
-            twoColumns={selectedConfigOptions.length > 5/>
-            </h2>
-            
-        </div>
-    );
+  // Find selected options label
+  const optionConfig = options.find(o => o.key === selectedOption);
+  const optionLabel = optionConfig ? optionConfig.label : null;
+  return (
+    <div className={css.sectionFeatures}>
+      <h2>
+        <FormattedMessage
+          id="ListingPage.classtypeType"
+          values={{ classtype: optionLabel.toLowerCase() }}
+        />
+      </h2>
+    </div>
+  );
 };
 
-export default SectionClasstypeMaybe;
+export default SectionclasstypeMaybe;
