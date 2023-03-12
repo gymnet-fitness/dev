44672875146e9037821080e20571a4ed588eaf3d
gymnet-fitness <liz@gymnet-fitness.com>
2023-03-12 16:48:17 -0400

    Update routeConfiguration.js

--- src/routeConfiguration.js
+++ src/routeConfiguration.js
@@ -1,4 +1,4 @@
-primport React from 'react';
+import React from 'react';
 import loadable from '@loadable/component';
 import getPageDataLoadingAPI from './containers/pageDataLoadingAPI';
 import { NotFoundPage } from './containers';
