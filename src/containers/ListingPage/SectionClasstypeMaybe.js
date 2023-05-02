1479664cb48bb71982867f85a94f2e68345effd5
gymnet-fitness <liz@gymnet-fitness.com>
2023-05-02 00:10:41 -0400

    Update SectionClasstypeMaybe.js

--- src/containers/ListingPage/SectionClasstypeMaybe.js
+++ src/containers/ListingPage/SectionClasstypeMaybe.js
@@ -1,4 +1,4 @@
-﻿import React from 'react';
+import React from 'react';
 import { FormattedMessage } from '../../util/reactIntl';
 
 // Import css from existing CSS Modules file:
@@ -9,27 +9,27 @@ const SectionClasstypeMaybe = props => {
   // Component's props should include all the possible options (from config)
   // and listing's publicData
   const { options, publicData } = props;
-  const selectedOption =
-    publicData && publicData.classtype ? publicData.classtype : null;
 
   // Don't return anything if public data doesn't contain classtype field
   // That's why we named this component as SectionclasstypeMaybe
-  if (!publicData || !selectedOption) { 
+  if (!publicData) { 
     return null;
   }
 
-  // Find selected options label
-  const optionConfig = options.find(o => o.key === selectedOption);
-  const optionLabel = optionConfig ? optionConfig.label : null;
+const selectedOptions = publicData && publicData.classtype ? publicData.classtype : [];
+    const selectedConfigOptions = options.filter(o => selectedOptions.find(s => s === o.key));
+
   return (
     <div className={css.sectionFeatures}>
-      <h2 className={css.featuresTitle}>
-        <FormattedMessage
-          id="ListingPage.classtypeTitle"
-          values={{ optionLabel }}
-        />
+      <h2 className={css.featuresTitle}
+	   <FormattedMessage id="ListingPage.classtypeTitle" />
       </h2>
-      
+      <PropertyGroup
+                id="ListingPage.classtype"
+                options={selectedConfigOptions}
+                selectedOptions={selectedOptions}
+                twoColumns={selectedConfigOptions.length > 5}
+            />
       
     </div>
   );
